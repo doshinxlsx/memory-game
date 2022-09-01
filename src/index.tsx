3298cd9a2cd85adee1813fa957449692bac03758
Dominik Orincsai <dominik.orincsai@wecanconsulting.eu>
2022-09-01 15:37:56 +0200

    changed default font to Roboto

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
+import theme from './utils/theme'
 import { App } from './App'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const container = document.getElementById('root')
 if (!container) throw new Error('Failed to find the root element')
 const root = ReactDOM.createRoot(container)
 
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ChakraProvider>,
 )
